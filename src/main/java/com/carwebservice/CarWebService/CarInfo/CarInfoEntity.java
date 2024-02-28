3c2cd3eeed54ea0ec5d6d66f9514c421b947f00c
Maciech <maciek.chodacki@wp.pl>
2024-02-28 22:38:39 +0100

    CWSB-0001 - Remote master project creation

--- src/main/java/com/carwebservice/CarWebService/CarInfo/CarInfoEntity.java
+++ src/main/java/com/carwebservice/CarWebService/CarInfo/CarInfoEntity.java
@@ -1,6 +1,5 @@
 package com.carwebservice.CarWebService.CarInfo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
